f300441829ec7609db9c5ded22dde52b97a0181e
Camila Morales <camila.moralesg21@gmail.com>
2022-05-04 14:05:32 -0500

    proyecto base - se realiza una función para agregar usuarios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import UserTable from './components/UserTable';
 import { v4 as uuidv4 } from 'uuid';
+import AddUserForm from './components/AddUserForm';
 
 
 function App() {
@@ -14,12 +15,22 @@ function App() {
   //state
   const [users, setUsers] = useState(usersData);
 
+  // Funcion Agregar usuario
+  const addUser = (user) =>{
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser ={addUser}/>
         </div>
         <div className="flex-large">
           <h2>View users</h2>
